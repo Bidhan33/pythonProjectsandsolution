8da32bcf04b6837e53f850bcd31943915e887500
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2024-05-22 12:10:13 +0300

    soln

---
+++ Number-if_else.py
@@ -0,0 +1,12 @@
+
+number = input("Select a number (1-3): ")
+
+
+if number == '1':
+    print("You selected one.")
+elif number == '2':
+    print("You selected two.")
+elif number == '3':
+    print("You selected three.")
+else:
+    print("Invalid selection. Please select a number between 1 and 3.")
